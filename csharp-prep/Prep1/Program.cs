0e3ed3a500665521a1534db83c5baa75719eb50e
lulusthrall <gal21021@byui.edu>
2026-02-19 20:15:15 -0700

    learning04

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,16 +1,19 @@
 using System;
+using System.Runtime.ExceptionServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your first name? ");
+        //Ask user for first name
+        Console.Write("What is your first name? ");
         string first = Console.ReadLine();
 
-        Console.Write("What is your last name? ");
+        //Ask the user for the last name
+        Console.Write("What is your last name? " );
         string last = Console.ReadLine();
 
         Console.WriteLine($"Your name is {last}, {first} {last}.");
-        
+        Console.ReadKey();
     }
 }
